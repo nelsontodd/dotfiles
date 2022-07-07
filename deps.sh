475f503da017a044240c94534e6605aa3f2d4d33
Nelson Morrow <nelsonmorrow@utexas.edu>
2022-07-07 23:02:09 +0000

    Tmux, nvim support

--- deps.sh
+++ deps.sh
@@ -3,6 +3,7 @@
 
 sudo apt-get install neovim
 sudo apt-get install tmux
+sudo apt-get install tree
 sudo apt-get install python3-pip
 pip3 install powerline-shell
 wget "https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb"
