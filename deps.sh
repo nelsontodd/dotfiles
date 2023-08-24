b06abd52403a28e81737a955166130a3150e8451
nelsontodd <nelsonmorrow@utexas.edu>
2023-08-24 04:26:32 +0000

    Nerd font

--- deps.sh
+++ deps.sh
@@ -8,3 +8,6 @@ sudo apt-get install python3-pip
 pip3 install powerline-shell
 wget "https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb"
 sudo dpkg -i vivid_0.8.0_amd64.deb
+#From https://github.com/ryanoasis/nerd-fonts#option-3-install-script
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
